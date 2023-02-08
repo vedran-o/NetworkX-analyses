4350916bb673480019766e22afb30155cf4319e9
vedran-o <96542493+vedran-o@users.noreply.github.com>
2023-02-08 23:00:24 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+from random import sample
+import networkx as nx
+import pandas as pd
+
+"""
+Moguća istraživačka pitanja:
+
+1)  Tko je znanstvenik HUB na Sveučilištu u Rijeci i kako izgleda njegov ego- graf?
+
+2)  Primjenom metoda za detekciju zajednica pokazati grupiraju li se znanstvenici u zajednice (znanstvene kolaboracije)
+na isti način kako su znanstvenici primarno grupirani (tj. zaposleni) na matične fakultete. Drugim riječima,
+surađuju li međusobno znanstvenici/profesori koji rade unutar istog fakulteta više ili manje nego što surađuju s
+profesorima koji su zaposleni na nekim drugim fakultetima? Itd.
+"""
+
+"""
+
+"""
+
+link = ('https://github.com/dnllvrvz/Social-Network-Dataset/raw/master/Social%20Network%20Dataset.xlsx')
+
+
